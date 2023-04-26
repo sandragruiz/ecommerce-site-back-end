30119837e7e6b91d6538597d4714dbca3e503526
unknown <sandra.gutierrez.ru@gmail.com>
2023-04-26 12:14:21 -0500

    defined model columns and relationships, synced database with sequelize

--- models/index.js
+++ models/index.js
@@ -1,16 +1,27 @@
 // import models
-const Product = require('./Product');
-const Category = require('./Category');
-const Tag = require('./Tag');
-const ProductTag = require('./ProductTag');
+const Product = require('./Product')(sequelize, Sequelize);
+const Category = require('./Category')(sequelize, Sequelize);
+const Tag = require('./Tag')(sequelize, Sequelize);
+const ProductTag = require('./ProductTag')(sequelize, Sequelize);
 
 // Products belongsTo Category
+Product.belongsTo(Category);
 
 // Categories have many Products
+Category.hasMany(Product);
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, { through: ProductTag });
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, { through: ProductTag });
+
+// Sync database
+sequelize.sync()
+  .then(() => {
+    console.log('Database synced');
+  });
+
 
 module.exports = {
   Product,
